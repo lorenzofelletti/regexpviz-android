580ee8411d1af5425f685901ee8ea101e1235f76
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2022-06-28 20:14:09 +0200

    fixed bug

--- app/src/main/java/com/lorenzofelletti/regexpviz/RegexMatchesSyntaxHighligher.kt
+++ app/src/main/java/com/lorenzofelletti/regexpviz/RegexMatchesSyntaxHighligher.kt
@@ -17,27 +17,32 @@ class RegexMatchesSyntaxHighlighter(
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 
     override fun afterTextChanged(s: Editable?) {
-        if (editTextRegex.text.toString().isEmpty())
-            return
-
-        val regex = Regex.fromLiteral(editTextRegex.text.toString())
-        val matchesSequence = regex.findAll(editTextTestString.text.toString(), 0)
 
         val testStringEditable = editTextTestString.text
+
+        // clears the outputs of the previous search
         testStringEditable.setSpan(
             BackgroundColorSpan(Color.TRANSPARENT),
             0,
             testStringEditable.toString().length,
             Spannable.SPAN_INCLUSIVE_INCLUSIVE
         )
+
+        if (editTextRegex.text.toString().isEmpty())
+            return
+
+        val regex = Regex.fromLiteral(editTextRegex.text.toString())
+        val matchesSequence = regex.findAll(editTextTestString.text.toString(), 0)
+
         matchesSequence.forEach {
             if (it.range.last > 0) {
-                val last =
-                    if (it.range.last + 1 > testStringEditable.length) it.range.last else it.range.last + 1
+                //val last =
+                //    if (it.range.last + 1 > testStringEditable.length) it.range.last else it.range.last + 1
                 testStringEditable.setSpan(
                     BackgroundColorSpan(Color.CYAN),
                     it.range.first,
-                    last,
+                    //last,
+                    it.range.last + 1,
                     Spannable.SPAN_INCLUSIVE_INCLUSIVE
                 )
             }
